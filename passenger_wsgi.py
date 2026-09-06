2ac6e8494d46d35bf1be02bde376866f107af933
Taffabella <nph44p7whv@privaterelay.appleid.com>
2026-09-06 18:18:54 +1000

    Sync all current project changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,11 @@
+import os
+import sys
+
+# 1. Add the virtual environment site-packages path
+sys.path.insert(0, '/home/thepione/virtualenv/DadJokeGenerator/3.10/lib/python3.X/site-packages')
+
+# 2. Add the directory where your app.py file lives
+sys.path.insert(0, '/home/thepione/DadJokeGenerator')
+
+# 3. Import your Flask app instance
 from app import app as application
